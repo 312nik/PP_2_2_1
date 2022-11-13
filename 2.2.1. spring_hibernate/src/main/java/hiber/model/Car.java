bdf09ef07d2e19d9bdc58e8faeaf59ea7991b06d
Moiseev Nikolay <niko-light@bk.ru>
2022-11-13 22:10:15 +0300

    на проверку

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -3,7 +3,7 @@ package hiber.model;
 import javax.persistence.*;
 
 @Entity
-@Table (name="car")
+@Table(name = "car")
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -15,8 +15,8 @@ public class Car {
     @Column(name = "series")
     private int series;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn (name="user_id")
+    @OneToOne
+    @JoinColumn(name = "user_id")
 
     private User user;
 
@@ -33,33 +33,48 @@ public class Car {
     }
 
     public Car(String model, int series) {
+
         this.model = model;
         this.series = series;
+
     }
 
     public String getModel() {
+
         return model;
     }
 
     public void setModel(String model) {
+
         this.model = model;
     }
 
     public int getSeries() {
+
         return series;
     }
 
     public void setSeries(int series) {
+
         this.series = series;
     }
 
-    public User getUser() { return user; }
+    public User getUser() {
+        return user;
+    }
 
-    public User setUser(User user) { this.user = user;return user;}
+    public User setUser(User user) {
+
+        this.user = user;
+        return user;
+
+    }
 
 
     @Override
     public String toString() {
-        return " владеет "+model+" "+series+"";
+
+        return " владеет " + model + " " + series + "";
+
     }
 }
